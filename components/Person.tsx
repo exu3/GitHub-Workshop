67a3f6789e03de5182e12694cebd939c3f525dff
eilla1 <exu6056@gmail.com>
2021-05-22 11:17:09 -0700

    chore: styles on person component and landing

--- components/Person.tsx
+++ components/Person.tsx
@@ -1,19 +1,19 @@
 export default function Person({ ghUsername, name, randomQuote }) {
   return (
     <a href={`https://github.com/${ghUsername}`} target="_blank">
-    <div className="shadow-md border border-gray-100 hover:shadow-2xl transition transition-delay-15 ease-in-out p-6 space-y-3 rounded-xl">
-      <div>
+      <div className="shadow-md border border-gray-100 hover:shadow-2xl transition transition-delay-15 ease-in-out p-6 space-y-3 rounded-xl">
+        <div>
           <img
             src={`https://avatars.githubusercontent.com/${ghUsername}`}
             alt={`${name}'s profile picture`}
-            className="w-32 h-32 rounded-full mx-auto"
+            className="w-32 h-32 rounded-full mx-auto border-4 border-pink"
           />
-          <p>{name}</p>
-          <p>{randomQuote}</p>
+          <h3 className="text-2xl font-sofia text-center font-semibold leading-loose">
+            {name}
+          </h3>
+          <p className="font-light italic text-center">"{randomQuote}"</p>
+        </div>
       </div>
-    </div>
     </a>
   );
 }
-
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,51 @@
 import { useEffect, useState } from "react";
 import Meta from "../components/head";
 import Person from "../components/Person";
-import ella from '../people/ella.json';
-
-
+import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
   const [thepeople, setThePeople] = useState([]);
-  useEffect(()=>{
-    fetch("/api/data").then((res)=>res.json())
-    .then((data)=>{setThePeople(data)
-    
-    }
+  useEffect(() => {
+    fetch("/api/data")
+      .then((res) => res.json())
+      .then((data) => {
+        setThePeople(data);
+      });
+  }, []);
 
-    )
-
-  },[])
-  
   return (
     <div>
       <Meta />
       <main className="m-8 md:mx-24">
-        <h1 className="font-sofia font-bold text-5xl leading-loose">
-          Virtual CodeDay: Intro to GitHub
-        </h1>
+        <section className="flex flex-col-reverse md:flex-row items-center justify-between">
+          <div>
+            <h1 className="font-sofia font-bold text-5xl py-8">
+              Virtual CodeDay: Intro to GitHub
+            </h1>
+          </div>
+          <div className="float-right">
+            <a
+              href="https://github.com/eilla1/CodeDay-GitHubWorkshop"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:text-gray-500"
+            >
+              <FaGithub size={45} />
+            </a>
+          </div>
+        </section>
         <section>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:grid-cols-3">
-            
-            {thepeople.map((person, i)=>{
-                return <Person key={i} name={person.name} ghUsername={person.githubUser} randomQuote={person.quote}/>
-                
+            {thepeople.map((person, i) => {
+              return (
+                <Person
+                  key={i}
+                  name={person.name}
+                  ghUsername={person.githubUser}
+                  randomQuote={person.quote}
+                />
+              );
             })}
-            
-            
-            
-            
           </div>
         </section>
       </main>
