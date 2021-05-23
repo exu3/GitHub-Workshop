9c79883c298256d4b14dff1ade9071c947682803
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2021-05-23 08:37:06 -0700

    tried working on navbar im not successful yet. topo not working so i just removed it and stuff yeah

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Meta from "../components/head";
 import Person from "../components/Person";
 import { FaGithub } from "react-icons/fa";
 
+
 export default function Home() {
   const [thepeople, setThePeople] = useState([]);
   useEffect(() => {
@@ -16,23 +17,30 @@ export default function Home() {
   return (
     <div>
       <Meta />
+      <nav className="border-b p-3 flex">
+      
+          <img className="justify-left" src="/codeday-text-color.svg" width="180px"/>
+          <div className="justify-right">
+                <a
+                  href="https://github.com/eilla1/CodeDay-GitHubWorkshop"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:text-gray-500"
+                >
+                  <FaGithub size={45} />
+                </a>
+              </div>
+      
+      </nav>
       <main className="m-8 md:mx-24">
         <section className="flex flex-col-reverse md:flex-row items-center justify-between">
           <div>
+          
             <h1 className="font-bold text-5xl py-8">
               Virtual CodeDay: Intro to GitHub
             </h1>
           </div>
-          <div className="float-right">
-            <a
-              href="https://github.com/eilla1/CodeDay-GitHubWorkshop"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-gray-500"
-            >
-              <FaGithub size={45} />
-            </a>
-          </div>
+          
         </section>
         <section>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:grid-cols-3">
