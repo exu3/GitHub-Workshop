ba8685f3dfd293e46a693cbbdd4672fae31f19ec
eilla1 <git@ella.cx>
2021-06-20 22:19:32 -0700

    fixes #23, move  to top level

--- website/pages/api/data.ts
+++ website/pages/api/data.ts
@@ -1,46 +1,30 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-interface ThePeople{
-
-    
-    name: string,
-    githubUser: string,
-    quote: string
-    
-    
-
+interface ThePeople {
+  name: string;
+  githubUser: string;
+  quote: string;
 }
 
 export default async (req, res) => {
-    
-    let data:ThePeople[]=[];
-    
-    
-    const files = fs.readdirSync(path.join(__dirname,"..", "..", "..", "..", "people"))
-    
-    files.forEach(file => {
-        const content = fs.readFileSync(path.join(__dirname,"..", "..", "..", "..", "people", file), "utf8")
-            
-            data.push(JSON.parse(content))
-            
-            
-        })
+  let data: ThePeople[] = [];
 
-        try{
-            data = data.sort(() => Math.random() - 0.5)
-        }catch(err){
+  const files = fs.readdirSync(
+    path.join(__dirname, "..", "..", "..", "..", "..", "people")
+  );
 
-        }
-        res.status(200).json(data)
-     
-    
-       
-       
-       
-    
-    
-}
-    
-    
-    
+  files.forEach((file) => {
+    const content = fs.readFileSync(
+      path.join(__dirname, "..", "..", "..", "..", "..", "people", file),
+      "utf8"
+    );
+
+    data.push(JSON.parse(content));
+  });
+
+  try {
+    data = data.sort(() => Math.random() - 0.5);
+  } catch (err) {}
+  res.status(200).json(data);
+};
